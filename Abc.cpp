2b4fe13a76f11108219e952d3964a2cecf7af903
Piyanshu05 <141199362+Piyanshu05@users.noreply.github.com>
2023-08-02 09:12:35 +0530

    Add files via upload

---
+++ Abc.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+printf("Name\v= \"Piyanshu\" \n");
+printf("Sem\v= \"Fir\bst\"\n");
+printf("Roll NO\v= \"141\" \n");
+printf("Address\v= \"Rajkot\?\"  \n");
+printf("\\ \420\n");
+printf("2\0");
+}
